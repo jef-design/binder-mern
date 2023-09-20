1bbf8cb391e5f16f90bea47964a48b354e3b87bc
Jeff Bermejo <77440578+jef-design@users.noreply.github.com>
2023-09-20 20:17:10 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,12 @@ const app = express()
 app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors({
+    origin: ["https://binder-mern.vercel.app"],
+    methods: ['POST', 'GET', 'DELETE','PATCH'],
+    credentials: true
+}))
+
 
 
 app.get('/', (req, res, next) => {
