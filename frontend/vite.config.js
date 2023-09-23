fda15feaf38a1c8ec6cbdcc71bb006e7082a59e2
Jeff Bermejo <77440578+jef-design@users.noreply.github.com>
2023-09-23 19:13:08 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api/binder': {
-        target: 'https://binder-api.onrender.com', // Replace with the URL of your backend server
-        changeOrigin: true,
-      },
-    },
-  },
+ 
 })
