c14495a638715ef622c3e7ca9749b63cf0128590
jef-design <bermejojff97@gmail.com>
2023-09-25 09:47:48 +0800

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,18 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api/binder': {
-        target: 'http://localhost:5000', // Replace with the URL of your backend server
-        changeOrigin: true,
-      },
+  // server: {
+  //   proxy: {
+  //     '/api/binder': {
+  //       target: 'http://localhost:5000', // Replace with the URL of your backend server
+  //       changeOrigin: true,
+  //     },
       
-    },
-  },
-=======
- plugins: [react()],
->>>>>>> 48cf4afe657d84e6207d8a49344ae202613d3ddf
+  //   },
+  // },
 })
